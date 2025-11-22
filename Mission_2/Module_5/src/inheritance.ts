630b7aafdc1a25fc0fa4e9f3e02574d2508abffa
MohammadRayhan190720 <rafirayhan260@gmail.com>
2025-11-22 08:31:58 +0600

    Leaing OOP Inheritance

--- Mission_2/Module_5/src/inheritance.ts
+++ Mission_2/Module_5/src/inheritance.ts
@@ -1,39 +1,45 @@
-  class Student {
-    name: string;
-    age: number;
-    address : string;
+    class Parent {
+      name: string;
+      age: number;
+      address: string;
 
-    constructor(name: string, age: number, address: string){
-      this.name = name ;
-      this.age = age;
-      this.address = address;
+      constructor(name: string, age: number, address: string) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+      }
+
+      getSleep(sleepHours: number) {
+        console.log(`${this.name} ato ${sleepHours}  ghumay`);
+      }
     }
-
-    getSleep(sleepHours : number){
-      console.log(`${this.name} ato ${sleepHours}  ghumay`)
+  
+  
+  
+  
+  
+  class Student extends Parent  {
+    roll:number;
+    constructor(name:string, age:number,address:string,roll:number){
+      super(name,age,address)
+      this.roll = roll
     }
   }
 
-  const student1 = new Student('kobir', 24 , "Rangpur")
+  const student1 = new Student('kobir', 24 , "Rangpur",101)
 
 student1.getSleep(7)
 
-  class Teacher {
-    name: string;
-    age: number;
-    address: string;
+
+  class Teacher extends Parent {
+
     designation: string;
 
     constructor(name: string, age: number, address: string,designation:string) {
-      this.name = name;
-      this.age = age;
-      this.address = address;
+      super(name,age,address)
       this.designation = designation;
     }
 
-    getSleep(sleepHours: number) {
-      console.log(`${this.name} ato ${sleepHours}  ghumay`);
-    }
 
     takeClass(classHour : number){
       console.log(`${this.name}  ${classHour} gonta class ney`)
